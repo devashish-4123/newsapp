529a1ef57542aa7160394a4309de9a0cb3f448d6
devashish-4123 <devashishjawale4123@gmail.com>
2024-05-13 12:24:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar/>
-        <Newscomp pageSize={10}/>
+        <Newscomp pageSize={8}/>
       </div>
     )
   }
